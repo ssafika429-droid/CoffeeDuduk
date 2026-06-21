f873895fdaf9c799a8166fa1472418a7c7f38739
Elis <ssafika429@gmail.com>
2026-06-21 15:52:21 +0700

    fix cors and auth endpoint

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json());
