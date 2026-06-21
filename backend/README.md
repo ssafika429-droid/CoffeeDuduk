c322e3141fda8895d665498818b7af930cb6b216
Elis <ssafika429@gmail.com>
2026-06-21 16:16:41 +0700

    fix API production URL

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+# CoffeeDuduk
+A responsive static website for Coffeeku Coffee Ordering System developed as a Midterm Project for Web Programming Course.
--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://coffeeduduk.onrender.com/api';
 
 export async function seedDatabase() {
   try {
